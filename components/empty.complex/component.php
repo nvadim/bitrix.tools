7cf8f83138cb7cfd5c9ceedefd01d9b73025c65c
Vadim Nazarov <nazarov-vadim@mail.ru>
2017-11-17 19:03:06 +0500

    complex edit

--- components/empty.complex/component.php
+++ components/empty.complex/component.php
@@ -1,31 +1,68 @@
 <?
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 
-$arComponentVariables = array('section_id', 'section_code', 'element_id',
-    'element_code', 'load', 'favorite', 'search');
-$arDefaultUrlTemplates404 = array('element' => 'w#element_id#/',
-    'section' => '#section_id#/',
-    'load' => 'load',
-    'favorite' => 'favorite',
-    'search' => 'search');
+//$arComponentVariables = array('section_id', 'section_code', 'element_id',
+//    'element_code', 'load', 'favorite', 'search');
+//$arDefaultUrlTemplates404 = array('element' => 'w#element_id#/',
+//    'section' => '#section_id#/',
+//    'load' => 'load',
+//    'favorite' => 'favorite',
+//    'search' => 'search');
+$arDefaultVariableAliases = array();
+$arDefaultUrlTemplates404 = array(
+    "sections" => "",
+    "section" => "#SECTION_ID#/",
+    "element" => "#SECTION_ID#/#ELEMENT_ID#/",
+    "compare" => "compare.php?action=COMPARE",
+    "smart_filter" => "filter/#SMART_FILTER_PATH#/apply/"
+);
+
+$arDefaultVariableAliases404 = array();
+
+$arComponentVariables = array(
+    "SECTION_ID",
+    "SECTION_CODE",
+    "ELEMENT_ID",
+    "ELEMENT_CODE",
+    "action",
+);
 
 if ($arParams['SEF_MODE'] == 'Y')
 {
     $arVariables = array();
-    $arUrlTemplates = CComponentEngine::MakeComponentUrlTemplates($arDefaultUrlTemplates404, $arParams['SEF_URL_TEMPLATES']);
-    $componentPage = CComponentEngine::ParseComponentPath($arParams['SEF_FOLDER'], $arUrlTemplates, $arVariables);
 
-    $arResult = array('VARIABLES' => $arVariables, 'ALIASES' => $arVariableAliases);
-    $arResult['PATH_TO_SECTION'] = $arParams['SEF_FOLDER'].$arParams['SEF_URL_TEMPLATES']['section'];
-    $arResult['PATH_TO_DETAIL'] = $arParams['SEF_FOLDER'].$arParams['SEF_URL_TEMPLATES']['element'];
-    $arResult['PATH_TO_LOAD'] = $arParams['SEF_FOLDER'].$arParams['SEF_URL_TEMPLATES']['load'];
-    $arResult['PATH_TO_FAVORITE'] = $arParams['SEF_FOLDER'].$arParams['SEF_URL_TEMPLATES']['favorite'];
-    $arResult['PATH_TO_SEARCH'] = $arParams['SEF_FOLDER'].$arParams['SEF_URL_TEMPLATES']['search'];
+    $engine = new CComponentEngine($this);
+    $arUrlTemplates = CComponentEngine::makeComponentUrlTemplates($arDefaultUrlTemplates404, $arParams["SEF_URL_TEMPLATES"]);
+    $arVariableAliases = CComponentEngine::makeComponentVariableAliases($arDefaultVariableAliases404, $arParams["VARIABLE_ALIASES"]);
+
+    $componentPage = $engine->guessComponentPath(
+        $arParams["SEF_FOLDER"],
+        $arUrlTemplates,
+        $arVariables
+    );
+
+    if(!$componentPage)
+    {
+        $componentPage = "index";
+    }
+
+
+    CComponentEngine::initComponentVariables($componentPage, $arComponentVariables, $arVariableAliases, $arVariables);
+    $arResult = array(
+        "FOLDER" => $arParams["SEF_FOLDER"],
+        "URL_TEMPLATES" => $arUrlTemplates,
+        "VARIABLES" => $arVariables,
+        "ALIASES" => $arVariableAliases
+    );
 }
 else
 {
-    $arVariableAliases = CComponentEngine::MakeComponentVariableAliases($arDefaultVariableAliases, $arParams['VARIABLE_ALIASES']);
-    CComponentEngine::InitComponentVariables(false, $arComponentVariables, $arVariableAliases, $arVariables);
+    $arVariables = array();
+
+    $arVariableAliases = CComponentEngine::makeComponentVariableAliases($arDefaultVariableAliases, $arParams["VARIABLE_ALIASES"]);
+    CComponentEngine::initComponentVariables(false, $arComponentVariables, $arVariableAliases, $arVariables);
+
+    $componentPage = "";
 
     if ($arVariables['element_id'].$arVariables['element_code'] != '')
         $componentPage = 'element';
@@ -40,14 +77,16 @@ else
     else
         $componentPage = 'index';
 
-    $arResult = array('VARIABLES' => $arVariables, 'ALIASES' => $arVariableAliases);
-    $arVarAliaces = $arParams['VARIABLE_ALIASES'];
-
-    $arResult['PATH_TO_SECTION'] = $APPLICATION->GetCurPageParam($arParams['VARIABLE_ALIASES']['section_id'].'=#section_id#', $arForDel);
-    $arResult['PATH_TO_DETAIL'] = $APPLICATION->GetCurPageParam($arParams['VARIABLE_ALIASES']['element_id'].'=#element_id#', $arForDel);
-    $arResult['PATH_TO_LOAD'] = $APPLICATION->GetCurPageParam($arParams['VARIABLE_ALIASES']['load'], $arForDel);
-    $arResult['PATH_TO_FAVORITE'] = $APPLICATION->GetCurPageParam($arParams['VARIABLE_ALIASES']['favorite'], $arForDel);
-    $arResult['PATH_TO_SEARCH'] = $APPLICATION->GetCurPageParam($arParams['VARIABLE_ALIASES']['search'], $arForDel);
+    $currentPage = htmlspecialcharsbx($APPLICATION->GetCurPage())."?";
+    $arResult = array(
+        "FOLDER" => "",
+        "URL_TEMPLATES" => array(
+            "intermediate" => $currentPage.$arVariableAliases["intermediate_num"]."=#intermediate_num#",
+            "page" => $currentPage.$arVariableAliases["page"] . '=#page#',
+        ),
+        "VARIABLES" => $arVariables,
+        "ALIASES" => $arVariableAliases
+    );
 }
 
 $this->IncludeComponentTemplate($componentPage);
